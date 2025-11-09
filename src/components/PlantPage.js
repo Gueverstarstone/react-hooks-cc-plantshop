721fd8f167ec5ab75a5153fa9a999662be2683a4
gueverstarstone <josephmungai028@gmail.com>
2025-11-09 11:58:01 +0300

    Added a search functionality to search plants by their names

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -2,14 +2,21 @@ import React from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+import { useState } from "react";
 
 //using plants as a prop in this page
 function PlantPage({ plants, onAddPlant }) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Filtering plants
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   return (
     <main>
       <NewPlantForm onAddPlant={onAddPlant} />
-      <Search />
-      <PlantList plants={plants} />
+      <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
